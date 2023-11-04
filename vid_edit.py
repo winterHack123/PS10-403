2e5ed22f1568cef08da6050961636d7b07a2b011
Zirtle <104186001+Zirtle@users.noreply.github.com>
2023-11-04 21:50:25 +0530

    x

--- vid_edit.py
+++ vid_edit.py
@@ -1,28 +1,29 @@
 import moviepy.editor as me
 import pandas as pd
 
-video_path = "downloads/Tom & Jerry ｜ Jerry in Full Force 🐭 ｜ Classic Cartoon Compilation ｜ @WB Kids.mp4"
-clip = me.VideoFileClip(video_path).without_audio()
+def create_vid(filename, res, ):
+    video_path = "downloads/" + filename
+    clip = me.VideoFileClip(video_path).without_audio()
 
-new_height = 720
-new_width = 9*new_height/16
+    new_height = res
+    new_width = 9*new_height/16
 
-speed = 2.0
+    speed = 1.25
 
-x1 = (clip.size[0] - new_width) / 2
-x2 = x1 + new_width
-y1 = (clip.size[1] - new_height) / 2
-y2 = y1 + new_height
+    x1 = (clip.size[0] - new_width) / 2
+    x2 = x1 + new_width
+    y1 = (clip.size[1] - new_height) / 2
+    y2 = y1 + new_height
 
-cropped_clip = clip.crop(x1=x1, y1=y1, x2=x2, y2=y2)
+    cropped_clip = clip.crop(x1=x1, y1=y1, x2=x2, y2=y2)
 
-x=10
-dur = 5
+    x=10
+    dur = 5
 
-clips = []
+    clips = []
 
-clip1 = cropped_clip.subclip(0,10).speedx(speed)
+    clip1 = cropped_clip.subclip(0,10).speedx(speed)
 
-clips.append(clip1)
+    clips.append(clip1)
 
-clip1.write_videofile("vertical_video.mp4", codec="libx264")
+    clip1.write_videofile("vertical_video.mp4", codec="libx264")
