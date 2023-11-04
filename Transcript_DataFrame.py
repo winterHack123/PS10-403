a5225c2cffa98972a07a584a73cc9f1715ff345b
aditya <ak942389@gmail.com>
2023-11-04 14:52:49 +0530

    Adding ending

--- Transcript_DataFrame.py
+++ Transcript_DataFrame.py
@@ -2,12 +2,16 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import openai
 import pandas as pd
 
-url = 'https://www.youtube.com/watch?v=2TL3DgIMY1g'
+url = 'https://www.youtube.com/watch?v=-Oc8Rgdb-7A'
 print(url)
 
 video_id = url.replace('https://www.youtube.com/watch?v=', '')
 print(video_id)
 
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
+print(transcript)
 df=pd.DataFrame(transcript)
+df['end'] = df['start'].shift(-1)
+df = df.drop(df.index[-1])
+df = df.drop('duration', axis=1)
 print(df)
