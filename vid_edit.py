6ed312e7d3bdbb7184e586f2cb46c3271acc6e0a
Zirtle <104186001+Zirtle@users.noreply.github.com>
2023-11-04 18:38:46 +0530

    more changes

--- vid_edit.py
+++ vid_edit.py
@@ -1,11 +1,13 @@
-from moviepy.editor import VideoFileClip
+import moviepy.editor as me
+import pandas as pd
 
 video_path = "downloads/Tom & Jerry ｜ Jerry in Full Force 🐭 ｜ Classic Cartoon Compilation ｜ @WB Kids.mp4"
-clip = VideoFileClip(video_path)
+clip = me.VideoFileClip(video_path).without_audio()
 
 new_height = 720
 new_width = 9*new_height/16
 
+speed = 2.0
 
 x1 = (clip.size[0] - new_width) / 2
 x2 = x1 + new_width
@@ -14,4 +16,13 @@ y2 = y1 + new_height
 
 cropped_clip = clip.crop(x1=x1, y1=y1, x2=x2, y2=y2)
 
-cropped_clip.write_videofile("vertical_video.mp4", codec="libx264")
+x=10
+dur = 5
+
+clips = []
+
+clip1 = cropped_clip.subclip(x, x + speed*dur).speedx(speed)
+
+clips.append(clip1)
+
+clip1.write_videofile("vertical_video.mp4", codec="libx264")
