b8c316ffa2eb522b3054ce6baaf57e62897e8da1
Zirtle <104186001+Zirtle@users.noreply.github.com>
2023-11-04 15:06:46 +0530

    Downloading yt video code

---
+++ yt_download.py
@@ -0,0 +1,26 @@
+import yt_dlp
+
+def download_youtube_videos(urls, output_folder="./downloads"):
+    ydl_opts = {
+        'outtmpl': f'{output_folder}/%(title)s.%(ext)s',
+    }
+    
+    downloaded_files = []
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        for url in urls:
+            info_dict = ydl.extract_info(url, download=True)
+            if 'entries' in info_dict:
+                for entry in info_dict['entries']:
+                    downloaded_files.append(entry['title'] + '.' + entry['ext'])
+            else:
+                downloaded_files.append(info_dict['title'] + '.' + info_dict['ext'])
+    return downloaded_files
+
+video_urls = []
+video_urls.append(input())
+output_folder = "./downloads" 
+downloaded_files = download_youtube_videos(video_urls, output_folder)
+print("Downloaded files:")
+for file in downloaded_files:
+    print(file)
