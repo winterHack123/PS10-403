f05caafe836e3f6ad9660a452706483fe54ebfa5
Zirtle <104186001+Zirtle@users.noreply.github.com>
2023-11-05 00:04:59 +0530

    made it into func

--- Transcript_DataFrame.py
+++ Transcript_DataFrame.py
@@ -1,16 +1,15 @@
 from youtube_transcript_api import YouTubeTranscriptApi
-import openai
 import pandas as pd
 
-url = 'https://www.youtube.com/watch?v=-Oc8Rgdb-7A'
+def get_transcript(url):
+    video_id = url.replace('https://www.youtube.com/watch?v=', '')
 
-video_id = url.replace('https://www.youtube.com/watch?v=', '')
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
-
-df=pd.DataFrame(transcript)
-df['end'] = df['start'].shift(-1)
-df = df.drop(df.index[-1])
-df = df.drop('duration', axis=1)
-df['id'] = df.reset_index().index
-print(df)
+    df=pd.DataFrame(transcript)
+    df['end'] = df['start'].shift(-1)
+    df = df.drop(df.index[-1])
+    df = df.drop('duration', axis=1)
+    df['id'] = df.reset_index().index
+    
+    return df
