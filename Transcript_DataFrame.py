81185b7c8a4de494b6f5f445871861cf523f0993
Zirtle <104186001+Zirtle@users.noreply.github.com>
2023-11-04 22:47:01 +0530

    add id to dataframe

--- Transcript_DataFrame.py
+++ Transcript_DataFrame.py
@@ -3,15 +3,14 @@ import openai
 import pandas as pd
 
 url = 'https://www.youtube.com/watch?v=-Oc8Rgdb-7A'
-print(url)
 
 video_id = url.replace('https://www.youtube.com/watch?v=', '')
-print(video_id)
 
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
-print(transcript)
+
 df=pd.DataFrame(transcript)
 df['end'] = df['start'].shift(-1)
 df = df.drop(df.index[-1])
 df = df.drop('duration', axis=1)
+df['id'] = df.reset_index().index
 print(df)
